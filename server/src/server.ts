52000df142beb2a5c43db4718436dde701ca4905
André Luiz da Silva <andreluiz.ti.dev@gmail.com>
2020-06-03 15:55:15 -0300

    Dia 2 - Desenvolvendo as funcionalidades da api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,21 @@
-import express from 'express';
+import path from 'path';
+import express, { request, response } from 'express';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  response.json([
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'André'
-  ]);
-});
+// Configurando cors
+app.use(cors());
 
+// Configurando express para trabalhar com dados em formato JSON
+app.use(express.json());
+
+// Configurando nossas rotas
+app.use(routes);
+
+// Servindo arquivos estáticos
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+// Iniciando servidor
 app.listen(3333);
