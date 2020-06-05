27931e6ead380112d32704ac3a95fe34ae7534e0
André Luiz da Silva <andreluiz.ti.dev@gmail.com>
2020-06-05 15:29:17 -0300

    Dia 4 - Criado rotas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,8 @@ import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
-import Home from './src/pages/Home';
+import Router from './src/routes';
+import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,7 +25,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Home />
+      <Routes />
     </>
   );
 }
